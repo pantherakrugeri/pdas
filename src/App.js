62b02f35754e5809f6695eced20221dc8b950ef3
Christopher Mullins <pantherakrugeri@gmail.com>
2023-06-07 22:27:50 -0500

    add styling bulma framework

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from "./ProfileCard";
 import AlexaImage from "./images/alexa.png";
 import CortanaImage from "./images/cortana.png";
@@ -7,11 +8,27 @@ console.log(AlexaImage);
 function App() {
     return (
         <div>
-            <div>Personal Digital Assistants</div>
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <p className='title'>Personal Digital Assistants</p>
+                </div>
+            </section>
 
-            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage}/>
-            <ProfileCard title="Cortana" handle="@cortana12" image={CortanaImage}/>
-            <ProfileCard title="Siri" handle="@siri32" image={SiriImage}/>
+            <div className="container">
+                <section className="section">
+                <div className="columns">
+                    <div className="column is-4">
+                        <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+                    </div>
+                    <div className="column is-4">
+                        <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+                    </div>
+                    <div className="column is-4">
+                        <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+                    </div>
+                </div>
+                </section>
+            </div>
         </div>
     )
 }
